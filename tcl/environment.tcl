1f777572c99df4f24314db41c78fece7f7804938
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2021-10-02 02:26:24 +0200

    Set the u280 fpga part in the environment file

--- tcl/environment.tcl
+++ tcl/environment.tcl
@@ -1,6 +1,7 @@
 set g_vivado_version 2020.1 
 set g_current_vivado_version [version -short] 
-set g_fpga_part xcvu47p-fsvh2892-2L-e 
+set g_fpga_part xcu280-fsvh2892-2L-e
+set g_board_part "xilinx.com:au280:part0:1.1"
 set g_project_name aurora_dma_ip
 set g_root_dir    [pwd]                     
 set g_project_dir ${g_root_dir}/project    
